2b3fff0b01ab10f2818f6b59638a2b0108d4c219
Andrew Mcveigh <me@andrewmcveigh.com>
2013-06-14 15:20:36 +0200

    Add clojars as releases

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/pliant/lein-package"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories [["snapshots" {:url "https://clojars.org/repo/" :creds :gpg}]
+                 ["releases" {:url "https://clojars.org/repo/" :creds :gpg}]])
